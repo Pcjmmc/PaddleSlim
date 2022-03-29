adba33dfdbf8b159eea715dafd0605ce8de84e4a
guozhengxin <guozhengxin@baidu.com>
2022-03-29 16:24:52 +0800

    add api
    Change-Id: I7a58ee753b2b42591055b7409d0531adf04c8c10

---
+++ backend/rpc/xly_api.py
@@ -0,0 +1,56 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+import requests
+import json
+import sys
+from xly_open import xlyOpenApiRequest
+
+class Xly_Api():
+    """
+    xly api 
+    """
+    @staticmethod
+    def run_job(pipelineid, branch, ciType, commit, diy_params=None):
+        """
+        触发xly流水线
+        """
+        url = "https://xly.bce.baidu.com/open-api/ipipe/rest/v3/pipeline-builds?pipelineId={}".format(pipelineid)
+        #需要加密参数
+        param = 'pipelineId=%s' % pipelineid
+        data = dict()
+        headers = { "Content-Type": "application/json",
+                     "IPIPE-UID": "Paddle-bot"
+         }
+        data['branch'] = branch
+        data['ciType'] = ciType
+        data['params'] = json.dumps(diy_params)
+        json_data = json.dumps(data)
+        res = xlyOpenApiRequest().post_method(url, json_data, param, headers=headers)
+        return res
+     
+    @staticmethod
+    def get_job(pipelinebuildid):
+        """
+        依据构建id获取流水线构建详情
+        """
+        url = "https://xly.bce.baidu.com/open-api/ipipe/agile/pipeline/v1/pipelineBuild/{}".format(pipelinebuildid)
+        print("url=", url)
+        headers = { "Content-Type": "application/json",
+                     "IPIPE-UID": "Paddle-bot"
+         }
+        res = xlyOpenApiRequest().get_method(url, headers=headers)
+        #print(res.json())
+        print(res)
+        return res
+        
+         
+
+if __name__ == '__main__':
+     pipelineid = "22094"
+     branch =  "develop"
+     ciType = "MERGE"
+     commit ="93a2f5652fa632e4eff8febf49304d64ec72b569"
+     diy_params = {"IPIPE_WHL_PATH":"hello"}
+     #Xly_Api().run_job(pipelineid, branch, ciType, commit, diy_params)
+     Xly_Api().get_job(5248008)
