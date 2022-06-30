a6a0c0d7eb1ac4894dd4cdee15c9f26357297ed7
liuhuanling <liuhuanling@baidu.com>
2022-06-30 14:22:41 +0800

    安装之后去掉代理
    Change-Id: Ibcc4ccf2e6057246cde40c351aad7ec3bb75fcc8

--- backend/start.sh
+++ backend/start.sh
@@ -1,10 +1,14 @@
 docker run -d --name=paddle_quality_web_backend  --net=host \
     -v /ssd1/app/paddletest/backend:/home/app/backend\
     -v /etc/localtime:/etc/localtime\
+    -e "http_proxy=http://172.19.57.45:3128" \
+    -e "https_proxy=http://172.19.57.45:3128" \
     -w /home/app/backend \
     ce_web_backend:backend_v1\
     /bin/bash -c "
     cp /home/app/backend/nginx.conf  /etc/nginx/nginx.conf
     pip install -r requirements.txt
+    unset https_proxy
+    unset http_proxy
     /usr/local/bin/supervisord -n -c /home/app/backend/supervisor/supervisord.conf
     "
