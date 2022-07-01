ede6ac4c27cf6c27b45eacf46918ba084217a9f1
liuhuanling <liuhuanling@baidu.com>
2022-07-01 14:44:25 +0800

    fix code style
    Change-Id: I00a3833745dc24fd1a32ec92ae40479b8de3687c

--- backend/app/ce/benchmark/cache.py
+++ backend/app/ce/benchmark/cache.py
@@ -1,3 +1,8 @@
+# !/usr/bin/env python3
+# encoding: utf-8
+"""
+benchmark的缓存定义
+"""
 import json
 
 import aioredis
@@ -6,12 +11,18 @@ from cache.caseBase import CacheBase
 
 
 class BenchmarkCase(CacheBase):
+    """
+    benchmark的缓存定义
+    """
     host = "paddle_quality"
     data_type = "string"
     key_format = 'api:benchmark:config'
 
     @classmethod
     async def set_value(cls, value):
+        """
+        set data
+        """
         if type(value) != str:
             value = json.dumps(value)
         with await cls.get_pools() as redis_conn:
@@ -19,6 +30,9 @@ class BenchmarkCase(CacheBase):
 
     @classmethod
     async def get_value(cls):
+        """
+        get data
+        """
         result = ""
         with await cls.get_pools() as redis_conn:
             res = await redis_conn.get(cls.key_format)
