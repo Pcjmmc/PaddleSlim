77aaff82aed60d114026c83d39d587dd4636ff18
liuhuanling <liuhuanling@baidu.com>
2022-07-01 15:16:10 +0800

    fix redis 版本
    Change-Id: Id5a4150b254ffeeca3736b23d7b66adc71f8dd2e

--- backend/start.sh
+++ backend/start.sh
@@ -1,12 +1,12 @@
 docker run -d --name=paddle_quality_web_backend  --net=host \
     -v /ssd1/app/paddletest/backend:/home/app/backend\
     -v /etc/localtime:/etc/localtime\
-    -e "http_proxy=http://172.19.57.45:3128" \
-    -e "https_proxy=http://172.19.57.45:3128" \
     -w /home/app/backend \
     ce_web_backend:backend_v1\
     /bin/bash -c "
     cp /home/app/backend/nginx.conf  /etc/nginx/nginx.conf
+    export http_proxy=http://172.19.57.45:3128
+    export https_proxy=http://172.19.57.45:3128
     pip install -r requirements.txt
     unset https_proxy
     unset http_proxy
