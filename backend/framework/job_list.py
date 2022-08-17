0300d42720872598bf4c7b8baad2865ef871f7cf
zhengtianyu <zhengtianyu@baidu.com>
2022-08-17 16:45:16 +0800

    fix bugs DLTP-54994
    Change-Id: I301f597d21adc3b14b17f7afb241eb336a1338ba

--- backend/framework/job_list.py
+++ backend/framework/job_list.py
@@ -25,37 +25,74 @@ class JobList(MABaseView):
         if id is not None:
             data =  await Job.aio_get_object(order_by=None, group_by=None, id=id)
             compile = await Compile.aio_get_object(id=data["compile"])
-            mission = dict()
-            check_complete = True
-            check_error = True
-            for k,v in json.loads(data["mission"]).items():
-                res = await Mission.aio_get_object(order_by=None, group_by=None, id=v)
-                mission[k] = {"id": v, "status": res["status"], "result": res["result"],
-                              "description": res["description"], "create_time": str(res["create_time"]),
-                              "update_time": str(res["update_time"]),}
-                if res["status"] != "done":
-                    check_complete = False
-                if res["status"] in ["done", "init", "running"]:
-                    check_error = False
-            if check_complete and data["status"] != "done":
-                await Job.aio_update({"status": "done", "update_time": datetime.now()}, {"id": id})
-                data = await Job.aio_get_object(order_by=None, group_by=None, id=id)
-            elif check_error and not check_complete:
+            # 编译失败
+            if compile["status"] not in ["done", "running"]:
                 await Job.aio_update({"status": "error", "update_time": datetime.now()}, {"id": id})
                 data = await Job.aio_get_object(order_by=None, group_by=None, id=id)
-            res_data = {
-                "id": data["id"],
-                "status": data["status"],
-                "compile": {
-                    "status": compile["status"],
-                    "wheel": compile["wheel"],
-                    "env": compile["env"],
-                    "create_time": str(compile["create_time"]),
-                    "update_time": str(compile["update_time"]),
-                },
-                "mission": mission,
-            }
-            return 1, res_data
+                res_data = {
+                    "id": data["id"],
+                    "status": data["status"],
+                    "compile": {
+                        "status": compile["status"],
+                        "wheel": compile["wheel"],
+                        "env": compile["env"],
+                        "create_time": str(compile["create_time"]),
+                        "update_time": str(compile["update_time"]),
+                    },
+                    "mission": data["mission"],
+                }
+                return 1, res_data
+            elif compile["status"] == "running":
+                data = await Job.aio_get_object(order_by=None, group_by=None, id=id)
+                res_data = {
+                    "id": data["id"],
+                    "status": data["status"],
+                    "compile": {
+                        "status": compile["status"],
+                        "wheel": compile["wheel"],
+                        "env": compile["env"],
+                        "create_time": str(compile["create_time"]),
+                        "update_time": str(compile["update_time"]),
+                    },
+                    "mission": data["mission"],
+                }
+                return 1, res_data
+            else:
+                check_complete = True
+                check_error = True
+                mission = dict()
+                for k,v in json.loads(data["mission"]).items():
+                    if v is None:
+                        check_complete = False
+                        check_error = False
+                        continue
+                    res = await Mission.aio_get_object(order_by=None, group_by=None, id=v)
+                    mission[k] = {"id": v, "status": res["status"], "result": res["result"],
+                                  "description": res["description"], "create_time": str(res["create_time"]),
+                                  "update_time": str(res["update_time"]),}
+                    if res["status"] != "done":
+                        check_complete = False
+                    if res["status"] in ["done", "init", "running"]:
+                        check_error = False
+                if check_complete and data["status"] == "running":
+                    await Job.aio_update({"status": "done", "update_time": datetime.now()}, {"id": id})
+                    data = await Job.aio_get_object(order_by=None, group_by=None, id=id)
+                elif check_error and not check_complete:
+                    await Job.aio_update({"status": "error", "update_time": datetime.now()}, {"id": id})
+                    data = await Job.aio_get_object(order_by=None, group_by=None, id=id)
+                res_data = {
+                    "id": data["id"],
+                    "status": data["status"],
+                    "compile": {
+                        "status": compile["status"],
+                        "wheel": compile["wheel"],
+                        "env": compile["env"],
+                        "create_time": str(compile["create_time"]),
+                        "update_time": str(compile["update_time"]),
+                    },
+                    "mission": mission,
+                }
+                return 1, res_data
         else:
             # 只返回查询列表
             page_index = kwargs.get("page_index")
