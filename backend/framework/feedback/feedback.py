a20c4107b1d2d2782f4f1dc47a3f631fd0c5815d
zhengtianyu <zhengtianyu@baidu.com>
2022-11-11 15:42:27 +0800

    paddletest-20 add feedback module
    Change-Id: I8cc8b43174b7a02e7c5f536b51a9cdb7fe77b4dd

---
+++ backend/framework/feedback/feedback.py
@@ -0,0 +1,41 @@
+#!/bin/env python3
+# -*- coding: utf-8 -*-
+# @author DDDivano
+# encoding=utf-8 vi:ts=4:sw=4:expandtab:ft=python
+import asyncio
+import json
+
+from views.base_view import MABaseView
+from models.framework import Job, Mission, Compile, FeedBack
+from exception import HTTP400Error
+from datetime import datetime
+from framework.dispatcher import Dispatcher
+import requests
+
+
+class FeedBackView(MABaseView):
+    """
+    云燊编译系统调用，构造缓存数据库
+    """
+    async def post(self, **kwargs):
+        return await super().post(**kwargs)
+
+    async def post_data(self, **kwargs):
+        """
+        feedback
+        """
+        # 构造入库数据
+        data = {}
+        data["uid"] = self._cookies.get("userid", 0)
+        data["username"] = self._cookies.get("username", 0)
+        data["content"] = kwargs.get("content")
+        data["star"] = kwargs.get("star")
+        data["create_time"] = datetime.now()
+        data["update_time"] = datetime.now()
+        res = await FeedBack.aio_insert(data)
+        if res[0] == 0:
+            raise HTTP400Error("数据写入失败")
+        else:
+            return "数据写入成功"
+
+
