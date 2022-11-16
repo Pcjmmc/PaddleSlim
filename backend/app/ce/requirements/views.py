56e0b28312acd2b97332060ab986716bc646ab18
guozhengxin <guozhengxin@baidu.com>
2022-11-16 11:45:27 +0800

    paddletest-39 add test detail from icafe
    Change-Id: Ibe2a3962ea8d12285debaee411e8db3ed7303efc

--- backend/app/ce/requirements/views.py
+++ backend/app/ce/requirements/views.py
@@ -137,6 +137,8 @@ async def get_cards_by_filter(page=1, page_num=20, iql=None):
         rd_owner = {}
         qa_owner = {}
         properties = item.get("properties", [])
+        repo = ""
+        pr = ""
         for arr in properties:
             if arr.get("propertyName") == "QA负责人":
                 qa_owner["name"] = arr.get("displayValue")
@@ -144,6 +146,13 @@ async def get_cards_by_filter(page=1, page_num=20, iql=None):
             elif arr.get("propertyName") == "RD负责人":
                 rd_owner["name"] = arr.get("displayValue")
                 rd_owner["username"] = arr.get("value")
+            elif arr.get("propertyName") == "repo":
+                 repo = arr.get("displayValue") if arr.get("displayValue") else ""
+            elif arr.get("propertyName") == "PR链接":
+                 pr_link = arr.get("value") 
+                 if pr_link:
+                     pr = pr_link.split("/")[-1]
+        print(item.get('sequence'), "pr=", pr, "repo=", repo)         
         #获取提测信息，order by updated time
         query_params = {}
         query_params["icafe_id"] = item.get('sequence')
@@ -161,6 +170,8 @@ async def get_cards_by_filter(page=1, page_num=20, iql=None):
             "rd_owner": rd_owner,
             "qa_owner": qa_owner,
             "page_size": page_size,
+            "repo": repo,
+            "pr": pr,
             "currnet_page": current_page,
             "test_id":  test_info.get("test_id") if test_info.get("test_id") else "",
             "test_status" : test_info.get("test_status") if test_info.get("test_status") else "",
@@ -220,11 +231,11 @@ class ProjectManage(MABaseView):
             kwargs.pop("method")
         if not icafe_id:
            return {}
-        count, project_id = await Project.aio_insert(validated_data=kwargs)
-        print("project_id=", project_id)
+        if method == "测试":
+            count, project_id = await Project.aio_insert(validated_data=kwargs)
+            print("project_id=", project_id)
         kwargs["method"] = method
         await update_icafe(**kwargs)
-        return {"id" : project_id}
 
     async def put(self, **kwargs):
         """
@@ -260,16 +271,21 @@ async def update_icafe(**kwargs):
     #TOTO 梳理卡片required字段更新对应icafe卡片
     #验证，如果缺少required字段,更新卡片状态会失败
     rd = kwargs.get("rd")
+    qa = kwargs.get("qa")
     test_id = kwargs.get("test_id")
     icafe_id = kwargs.get("icafe_id")
     test_status = kwargs.get("test_status")
     method = kwargs.get("method")
+    operator = ""
     target = None
     if method == "提测":
         target = "测试中"
+        operator = rd
     elif method == "测试":
         target = "测试中"
+        operator = qa
     elif method == "确认":
+        operator = qa
         target = "测试完成"
     else:
         return 
@@ -287,7 +303,7 @@ async def update_icafe(**kwargs):
         fields_list.append("repo={}".format(repo))
     if pr:
         if repo and  "pull" not in pr:
-             pr = "github.com/PaddlePaddle/Paddle/pull/" + pr 
+             pr = "github.com/PaddlePaddle/{}/pull/{}".format(repo, pr) 
         fields_list.append("PR链接={}".format(pr))
     if not icafe_id and test_id:
        #TODO查询DB获取所有id
@@ -297,7 +313,7 @@ async def update_icafe(**kwargs):
         'u': PADDLE_ICAFE_USER,
         'pw': PADDLE_ICAFE_PASSD,
         'isCheckStatus': False,
-        'operator' : rd,
+        'operator' : operator,
         'fields': fields_list
     }).get_data(**{"card_id":icafe_id})
  
