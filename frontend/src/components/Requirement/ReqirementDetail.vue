694eb7a22acbf4f74342d0692e5330f3f8315bf0
liuhuanling <liuhuanling@baidu.com>
2022-11-17 11:03:39 +0800

    paddletest-38 优化需求页
    Change-Id: I98b2824b65978763452837be3fb844e7c32b558c

--- frontend/src/components/Requirement/ReqirementDetail.vue
+++ frontend/src/components/Requirement/ReqirementDetail.vue
@@ -2,31 +2,32 @@
   <div>
    <Card
       class="center-card-s"
-      v-if="datas.length > 0"
     >
       <div>
         <Row style="margin-top: 1%;">
           <Col>
-            需求title: {{ $route.query.title }}
+            需求title: {{ content.title }}
           </Col>
           <Col offset="1">
-            RD: {{ $route.query.rd }}
+            RD: {{ content.rd_owner }}
           </Col>
           <Col offset="1">
-            QA: {{ $route.query.qa }}
+            QA: {{ content.qa_owner }}
           </Col>
           <Col offset="1">
-          Repo: {{ $route.query.repo }}
+          Repo: {{ content.repo }}
           </Col>
           <Col offset="1">
-          pr: {{ $route.query.pr }}
+          pr: 
+          <a href="javascript:void(0)" @click="jumper(content.pr)">
+            {{ content.pr }}
+          </a>
           </Col>
         </Row>
       </div>
     </Card>
    <Card
       class="center-card-s"
-      v-if="datas.length > 0"
     >
       <div>
         <div>
@@ -39,11 +40,28 @@
         </div>
       </div>
     </Card>
+    <Modal
+      v-model="showApproveModa"
+      title="确认结果"
+      width="30%"
+      v-on:on-cancel="handleReset"
+    >
+      <RadioGroup v-model="approve">
+        <Radio label="pass">成功</Radio>
+        <Radio label="fail">失败</Radio>
+      </RadioGroup>
+      <div slot="footer">
+        <Button type="text" @click="handleReset">取消</Button>
+        <Button type="primary" @click="handleSubmit">提交</Button>
+      </div>
+    </Modal>
   </div>
 </template>
 
 <script>
-import { RequriementDetailUrl } from '../../api/url.js';
+import Cookies from 'js-cookie';
+import { dateFmt, timestampToTime } from '../../util/help.js';
+import { RequriementDetailUrl, StartTestUrl, UserCheckUrl, AssociateBugUrl } from '../../api/url.js';
 import api from '../../api/index';
 
 export default {
@@ -51,6 +69,13 @@ export default {
   props: {},
   data: function () {
     return {
+      userInfo: {
+        identifyQA: false,
+        username: Cookies.get('username')
+      },
+      content: null,
+      showApproveModa: false,
+      approve: 'pass',
       datas: [],
       columns: [
         {
@@ -79,13 +104,23 @@ export default {
         {
           title: '测试时间',
           key: 'created',
+          align: 'center',
+          render: (h, params) => {
+            return h('div', [
+              h('p', {
+              }, this.changeTimestamp(params.row.created))
+            ]);
+          }
+        },
+        {
+          title: '测试结论',
+          key: 'approve',
           align: 'center'
         },
         {
-          title: '详细结果',
+          title: '测试项详情',
           key: 'report',
           align: 'center',
-          fixed: 'right',
           render: (h, params) => {
             let ret = [];
             ret.push(
@@ -115,11 +150,75 @@ export default {
               ret
             );
           }
+        },
+        {
+          title: '操作',
+          key: 'operation',
+          align: 'center',
+          width: '100px',
+          fixed: 'right',
+          render: (h, params) => {
+            let ret = [];
+            if (this.userInfo.identifyQA && this.userInfo.username === this.content.qa_username) {
+              ret.push(
+              h(
+                'Button',
+                {
+                  props: {
+                    type: 'primary',
+                    size: 'small'
+                  },
+                  style: {
+                    marginRight: '5px'
+                  },
+                  on: {
+                    click: () => {
+                      this.setJobStatus(params.row);
+                    }
+                  }
+                },
+                '确认结果'
+              )
+            );
+            } else {
+              ret.push(
+                h(
+                  'Button',
+                  {
+                    props: {
+                      type: 'primary',
+                      size: 'small',
+                      disabled: true
+                    },
+                    style: {
+                      marginRight: '5px'
+                    }
+                  },
+                  '确认结果'
+                )
+              );
+            }
+            return h(
+              'div',
+              {
+                style: {
+                  display: 'flex',
+                  flexWrap: 'wrap',
+                  justifyContent: 'flex-start',
+                  alignItems: 'flex-start'
+                }
+              },
+              ret
+            );
+          }
         }
       ]
     };
   },
   mounted: async function () {
+    this.initData();
+    await this.searchIcafeDetail();
+    await this.CheckUserInfo();
     await this.getDetails();
   },
   computed: {
@@ -127,8 +226,16 @@ export default {
   components: {
   },
   methods: {
+    changeTimestamp(timestamp, offset = 8) {
+      if (timestamp) {
+        let date = timestampToTime(timestamp, offset);
+        let dt = dateFmt(date, 'yyyy-MM-dd hh:mm:ss');
+        return dt;
+      } else {
+        return '';
+      }
+    },
     getStatus(status) {
-      console.log('status', status);
       switch (status.toLowerCase()) {
         case 'success':
           return '成功';
@@ -168,6 +275,36 @@ export default {
           return 'error';
       }
     },
+    async searchIcafeDetail() {
+      this.content = null;
+      let params = {
+        sequence: this.$route.params.reqid
+      };
+      const {code, data, message} = await api.get(AssociateBugUrl, params);
+      if (parseInt(code, 10) === 200) {
+        this.content = data[0];
+        console.log('icafe detail is', this.content);
+      } else {
+        this.content = null;
+        this.$Message.error({
+          content: '请求出错: ' + message,
+          duration: 30,
+          closable: true
+        });
+      }
+    },
+    async CheckUserInfo() {
+      const { code, data, message } = await api.get(UserCheckUrl);
+      if (parseInt(code, 10) === 200) {
+        this.userInfo.identifyQA = data.identifyQA;
+      } else {
+        this.$Message.error({
+          content: '请求出错: ' + message,
+          duration: 30,
+          closable: true
+        });
+      }
+    },
     async getDetails() {
       this.datas = [];
       let params = {
@@ -186,9 +323,46 @@ export default {
         });
       }
     },
-    jumper() {
+    jumper(url) {
       // 跳转到相应的需求页,自己的工作台 todo
-      console.log('desc', this.req);
+      window.open(url, '_blank');
+    },
+    setJobStatus(row) {
+      this.selectRow = row;
+      this.showApproveModa = true;
+    },
+    async handleReset() {
+      this.initData();
+    },
+    initData() {
+      this.showApproveModa = false;
+      this.approve = 'pass';
+      this.selectRow = null;
+    },
+    async handleSubmit() {
+      await this.TestJobDone();
+      this.initData();
+    },
+    // 标记icafe测试完成
+    async TestJobDone(row) {
+      let params = {
+        method: '确认',
+        icafe_id: this.$route.params.reqid,
+        rd: this.$route.query.rd,
+        qa: this.$route.query.qa,
+        test_id: this.selectRow.test_id,
+        approve: this.approve
+      };
+      const { code, message } = await api.put(StartTestUrl, params);
+      if (parseInt(code, 10) === 200) {
+        await this.getDetails();
+      } else {
+        this.$Message.error({
+          content: '请求出错: ' + message,
+          duration: 30,
+          closable: true
+        });
+      }
     },
     async getTestJobDetail(item) {
       // 获取最新一次任务的进度
