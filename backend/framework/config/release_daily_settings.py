218f0922763c62c11f68937d9d8deb555c41e675
zhengtianyu <zhengtianyu@baidu.com>
2023-02-21 11:03:11 +0800

    add two super user DLTP-54994
    Change-Id: Ie54d4c7fb18cc3e1345db34a43af4ac5d8641a66

--- backend/framework/config/release_daily_settings.py
+++ backend/framework/config/release_daily_settings.py
@@ -8,7 +8,7 @@ for icode
 
 # 发版风险日报数据库配置信息
 
-RELEASE_DAILY_SUPERUSER = ["zhengtianyu", "liuhuanling"]
+RELEASE_DAILY_SUPERUSER = ["zhengtianyu", "liuhuanling", "zhangdeying", "weike"]
 
 RELEASE_DAILY_SETTINGS = [
     {
@@ -31,5 +31,39 @@ RELEASE_DAILY_SETTINGS = [
         "order": 4,
         "owner": "liuxudong04,zhangyulong04,yubaoku",
     },
-
+    {
+        "module": "模型Benchmark",
+        "order": 5,
+        "owner": "guomengmeng01,guolixin,xiangyihua",
+    },
+    {
+        "module": "PaddleClas & PaddleGAN 套件",
+        "order": 6,
+        "owner": "liuquanxiang",
+    },
+    {
+        "module": "PaddleOCR & PaddleSpeech & Paddle3D 套件",
+        "order": 7,
+        "owner": "jiaxiao01",
+    },
+    {
+        "module": "PaddleSeg & PaddleDetection 套件",
+        "order": 8,
+        "owner": "xiege01",
+    },
+    {
+        "module": "PaddleNLP 套件",
+        "order": 9,
+        "owner": "zhangjunjun04",
+    },
+    {
+        "module": "PaddleScience 套件",
+        "order": 10,
+        "owner": "suijiaxin",
+    },
+    {
+        "module": "PaddleSlim & PaddleRec 套件 & 新硬件",
+        "order": 11,
+        "owner": "wanghongtao",
+    },
 ]
