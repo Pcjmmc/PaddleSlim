656196ffb97ef0832c5b478946413258dcc562f2
zhengtianyu <zhengtianyu@baidu.com>
2023-03-14 10:09:50 +0800

    support webhook DLTP-54994
    Change-Id: I9bf31c74cbe0523cf496c8c8420c20d89f491e67

--- backend/framework/mission_pkg/mission_callback.py
+++ backend/framework/mission_pkg/mission_callback.py
@@ -20,6 +20,13 @@ class MissionCallback(MABaseView):
     """
     mission 回调函数
     """
+    #Todo： 效率云回调内容不明确，使用MAPPING映射，防止后面的改动太大
+    STATUS_MAPPING = {
+        "PENDING": "running",
+        "RUNNING": "running",
+        "SUCC": "done",
+        "FAIL": "fail",
+    }
     async def post(self, **kwargs):
         return await super().post(**kwargs)
 
@@ -30,21 +37,32 @@ class MissionCallback(MABaseView):
         id = kwargs.get("id", None)
         pipelineid = kwargs.get("AGILE_PIPELINE_BUILD_ID")
         kwargs["update_time"] = datetime.now()
-        #  Todo：解析数据 self.request.headers.get() 等对齐数据结构就OK
+        #  Todo：解析数据 self.request.headers.get() 等对齐数据结构就OK「
+        TRIGGER_TYPE = self.request.headers.get("TRIGGER_TYPE", None)
         if id is not None:
             res = await Mission.aio_update(kwargs, {"id": id})
-        else:
-            exit_code = self.request.headers.get("JOB_EXIT_CODE")
+        elif TRIGGER_TYPE == "STATUS":
+            # Todo： 因为回调内容不明确，所以先做兼容性写法
+            exit_code = self.request.headers.get("JOB_EXIT_CODE", None)
+            if exit_code is None:
+                exit_code = kwargs.get("JOB_EXIT_CODE")
+            # Todo: TRIGGER_TYPE 必须是 STATUS才认定为自动回调
             if exit_code == "0":
                 # Todo: 对齐成功回调的数据内容，解析映射数据库字段，入库
-                data = {}
+                data = {
+                    "status": self.STATUS_MAPPING.get(self.request.headers.get("BUILD_STATUS")),
+                    "result": kwargs.get("result"),
+                    "allure_report": kwargs.get("allure_report")
+                }
             else:
                 # Todo: 根据退出码赋值
                 data = {
                     "status": "error",
                     "result": ERROR_MSG.get(exit_code),
                 }
-            res = await Mission.aio_update(kwargs, {"description": pipelineid})
+            res = await Mission.aio_update(data, {"description": pipelineid})
+        else:
+            raise HTTP400Error("非正常回调请求，通过TRIGGER_TYPE判断非自动触发回调")
         if res == 0:
             raise HTTP400Error("回调数据有问题")
         # get jid
