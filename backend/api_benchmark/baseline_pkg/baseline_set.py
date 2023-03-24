0ffb479f920187881fa4aa51d6d23ca2b98ab22b
luozeyu01 <luozeyu01@baidu.com>
2023-03-24 20:03:59 +0800

    DLTP-54994 add apibm super admin a2
    Change-Id: I08186945366c51bcfe390bfdbb9e5cffdcf548cc

--- backend/api_benchmark/baseline_pkg/baseline_set.py
+++ backend/api_benchmark/baseline_pkg/baseline_set.py
@@ -20,6 +20,7 @@ import datetime
 from exception import HTTP400Error
 from views.base_view import MABaseView
 from models.api_benchmark import Job
+from models.user import User
 from api_benchmark.config.service_url import API_BENCHMARK_SUPERUSER
 from utils.change_time import get_begin_and_time
 
