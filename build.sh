73d6ef2b495d29a4193967e6dfeb6fb2f76dadbb
zhengtianyu <zhengtianyu@baidu.com>
2023-03-24 15:21:18 +0800

    add build allure_go script DLTP-54994
    Change-Id: I3a035ee3fe9eb3a224094bca6c201f1d0a943401

--- build.sh
+++ build.sh
@@ -3,3 +3,11 @@
 mkdir output
 cp -R ./backend ./output/
 cp -R ./frontend ./output/
+
+
+# build allure_service
+cd allure_service
+go get -u github.com/gin-gonic/gin
+go get -u golang.org/x/sync/errgroup
+go build allure_service.go
+cp allure_service ../output
