4abfa59eed879e20eee76f680aa12a6aa7135d8b
wangzhe12 <wangzhe12@baidu.com>
2023-03-27 12:00:33 +0800

    tgqa-paas-1424 支持PaddleTest接入
    Change-Id: I739b26169eb4eb12634cdea814ddb82e3e9de4e3

--- build.sh
+++ build.sh
@@ -4,10 +4,13 @@ mkdir output
 cp -R ./backend ./output/
 cp -R ./frontend ./output/
 
-
 # build allure_service
 cd allure_service
+
+export GOENV=`pwd`/go.env
+
 go get -u github.com/gin-gonic/gin
 go get -u golang.org/x/sync/errgroup
 go build allure_service.go
-cp allure_service ../output
+
+cp allure_service ../output/
