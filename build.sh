97e116b1f3fbe9dcef33275ef64950dc3325e7b5
zhengtianyu <zhengtianyu@baidu.com>
2023-03-27 11:33:01 +0800

    support check 404 DLTP-54994
    Change-Id: I4a20f3ac8d1b0cea9cc20d0ca6dd6e551386243d

--- build.sh
+++ build.sh
@@ -6,11 +6,10 @@ cp -R ./frontend ./output/
 
 # build allure_service
 cd allure_service
-
 export GOENV=`pwd`/go.env
-
 go get -u github.com/gin-gonic/gin
 go get -u golang.org/x/sync/errgroup
 go build allure_service.go
+cp allure_service ../output
+cp nginx.conf ../output
 
-cp allure_service ../output/
