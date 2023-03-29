05535b5c23b4930a391265a7ef8f6be29edbbc01
zhengtianyu <zhengtianyu@baidu.com>
2023-03-29 15:43:05 +0800

    change framework dir TO /app DLTP-54994
    Change-Id: Ibd03d89e6d638806d387004dd07f650a094fa0ca

--- backend/app/framework/feedback/feedback.py
+++ backend/app/framework/feedback/feedback.py
@@ -9,7 +9,7 @@ from views.base_view import MABaseView
 from models.framework import Job, Mission, Compile, FeedBack
 from exception import HTTP400Error
 from datetime import datetime
-from framework.dispatcher import Dispatcher
+from app.framework.dispatcher import Dispatcher
 import requests
 
 
